c57adef8fa6f576d3a0daff6b5f23e5505d6e5b2
Jacob <88961633+jacobleft@users.noreply.github.com>
2020-05-27 14:10:03 +0800

    Initial commit.

---
+++ dynamics2.jl
@@ -0,0 +1,276 @@
+using SPARK
+using LinearAlgebra
+using StaticArrays
+using Revise
+using Vert
+function tail(n)
+    nver = n
+    nhor = n + 1
+    nb = nver + nhor
+    n_revolute = 2 + 2(n-1)
+    ver_lengths = zeros(nver)
+    hor_lengths = zeros(nhor)
+    ver_lengths .= 0.04
+    hor_lengths .= 0.04
+    O = zeros(2,nhor)
+    for i in 2:nhor
+        O[2,i] = O[2,i-1] - ver_lengths[i-1]
+    end
+    P = zeros(2,nhor)
+    for i in 1:nhor
+        P[:,i] = O[:,i] .+ [hor_lengths[i]/2,0.0]
+    end
+
+    m = fill(1.3e-2,nb)
+    L = zeros(nb)
+
+    ver_index = 2:2:nb
+    hor_index = 1:2:nb
+    for (i,j) in enumerate(ver_index)
+        L[j] = ver_lengths[i]
+    end
+    for (i,j) in enumerate(hor_index)
+        L[j] = hor_lengths[i]
+    end
+    CoM = [zeros(2) for i = 1:nb]
+    for (i,j) in enumerate(ver_index)
+        CoM[j] .= [ver_lengths[i]/2,0.0]
+    end
+    inertia = [zeros(2,2) for i = 1:nb]
+    for (i,j) in enumerate(ver_index)
+        inertia[j] .= [0.0 0.0; 0.0 m[j]*L[j]^2/3]
+    end
+    for (i,j) in enumerate(hor_index)
+        inertia[j] .= [0.0 0.0; 0.0 m[j]*L[j]^2/12]
+    end
+    ri = [zeros(2) for i = 1:nb]
+    rj = [zeros(2) for i = 1:nb]
+    for (i,j) in enumerate(ver_index)
+        ri[j] .= O[:,i]
+        rj[j] .= O[:,i+1]
+    end
+    for (i,j) in enumerate(hor_index)
+        ri[j] .= O[:,i]
+        rj[j] .= P[:,i]
+    end
+    p1 = [zeros(2) for i = 1:nb]
+    p2 = [zeros(2) for i = 1:nb]
+    for (i,j) in enumerate(ver_index)
+        p1[j] .= [0.0,0.0]
+        p2[j] .= [ver_lengths[i],0.0]
+    end
+    for (i,j) in enumerate(hor_index)
+        p1[j] .= [-hor_lengths[i]/2,0.0]
+        p2[j] .= [ hor_lengths[i]/2,0.0]
+    end
+    # aps = [p1[i],p2[i]]
+    # nap = length(aps)
+    # ap = [Vert.AnchorPoint2D(p) for p in aps]
+    #
+    # prop = Vert.RigidBody2DProperty(movable,name,type,
+    #             m[i],
+    #             SMatrix{2,2}(inertia[i]),
+    #             SVector{2}(CoM[i]),
+    #             SVector{nap}(ap)
+    #             )
+    # @code_warntype Vert.RigidBody2DProperty(movable,name,type,
+    #             m[i],
+    #             SMatrix{2,2}(inertia[i]),
+    #             SVector{2}(CoM[i]),
+    #             SVector{nap}(ap)
+    #             )
+    # state = Vert.RigidBody2DState(prop,ri[i],rj[i])
+    # @code_warntype Vert.RigidBody2DState(prop,ri[i],rj[i])
+    # rb = Vert.RigidBody2D(prop,state)
+    # @code_warntype Vert.RigidBody2D(prop,state)
+    function rigidbody(i,CoM,m,inertia,ri,rj,aps)
+        name = Symbol("rb"*string(i))
+        type = :generic
+        if i == 1
+            movable = false
+        else
+            movable = true
+        end
+        nap = 2
+        ap = [Vert.AnchorPoint2D(p) for p in aps]
+        prop = Vert.RigidBody2DProperty(movable,name,type,
+                    m,SMatrix{2,2}(inertia),
+                    SVector{2}(CoM),
+                    SVector{nap}(ap)
+                    )
+        state = Vert.RigidBody2DState(prop,ri,rj)
+        rb = Vert.RigidBody2D(prop,state)
+    end
+    i = 1
+    rb1 = rigidbody(i,CoM[i],m[i],inertia[i],ri[i],rj[i],[p1[i],p2[i]])
+    rbs = [rigidbody(i,CoM[i],m[i],inertia[i],ri[i],rj[i],[p1[i],p2[i]]) for i = 1:nb]
+
+    original_restlength = fill(SVector(0.04,√3*0.02,√3*0.02,0.04),n)
+    restlength = fill(MVector(0.04,√3*0.02,√3*0.02,0.04),n)
+    actuallength = fill(MVector(0.04,√3*0.02,√3*0.02,0.04),n)
+    k = fill(100.0,n)
+    vss = [Vert.DString(k[i],original_restlength[i],
+        restlength[i],actuallength[i],zeros(MVector{4})) for i = 1:n]
+    # @code_warntype   Vert.DString(k[i],original_restlength[i],
+    #         restlength[i],actuallength[i],zeros(MVector{4}))
+    rbs,vss
+end
+
+n = 4
+rbs,vss = tail(n)
+
+@code_warntype tail(4)
+
+function tailstructure(n)
+    rbs,vss = tail(n)
+
+    connectivity = [
+        ifelse(isodd(i),[i,i+1],[i-1,i+1]) for i = 1:length(rbs)
+    ]
+    p_connectivity = [
+        [2pid[1]-1,2pid[1],2pid[2]-1,2pid[2]] for pid in connectivity
+    ]
+
+    Vert.Structure2D(rbs,vss,p_connectivity)
+end
+tailstruct = tailstructure(4)
+@code_warntype tailstructure(4)
+
+function tail_spark(st2d)
+    rbs = st2d.rigidbodies
+    vss = st2d.strings
+    cnt = st2d.connectivity
+
+    ntotalbody = length(rbs)
+    mvbodyindex = collect(2:ntotalbody)
+    mvrigidbodies = rbs[mvbodyindex]
+    nbody = length(mvrigidbodies)
+    ninconstraint = ntotalbody
+    nfix = ntotalbody - nbody
+    nexconstraint = 3nfix
+    nconstraint = ninconstraint + nexconstraint
+    n_revolute = 2 + 2(n-1)
+    nq = 4ntotalbody - 2n_revolute
+    @show nq
+    #total_mass_matrix = zeros(4nbody,4nbody)
+    mass_matrix = zeros(nq,nq)
+    for (rbid,rb) in enumerate(rbs)
+        pindex = cnt[rbid]
+        mass_matrix[pindex,pindex] .+= rbs[rbid].state.auxs.M
+    end
+    function M!(mm,q)
+        mm .= mass_matrix
+    end
+
+    # System functions
+    function ∂T∂q̇!(p,M,q,q̇)
+        mul!(p,mass_matrix,q̇)
+    end
+
+    function F!(F,q,q̇,t)
+        vs1 = vss[1]
+        Δ = sin(t)
+        rls = vs1.restlengths
+        orls = vs1.original_restlengths
+        rls[1] = orls[1]*(1+0.2Δ)
+        rls[2] = orls[2]*(1+0.2Δ)
+        rls[3] = orls[3]*(1-0.2Δ)
+        rls[4] = orls[4]*(1-0.2Δ)
+        Vert.reset_forces!(rbs)
+        Vert.q2rbstate!(st2d,q,q̇)
+        Vert.update_forces!(st2d)
+        Vert.genforces!(rbs)
+        F .= 0.0
+        for (rbid,rb) in enumerate(rbs)
+            pindex = cnt[rbid]
+            F[pindex] .+= rb.state.auxs.Q
+        end
+    end
+
+    function Φ(q)
+        ret = Vector{eltype(q)}(undef,nconstraint)
+        for (rbid,rb) in enumerate(rbs)
+            pindex = cnt[rbid]
+            ret[3nfix+rbid] = rb.state.auxs.Φ(q[pindex])
+        end
+        xi,yi,xj,yj = q[1:4]
+        ret[1] = xi - 0.0
+        ret[2] = yi - 0.0
+        ret[3] = yj - 0.0
+        ret
+    end
+
+    function A(q)
+        ret = zeros(eltype(q),nconstraint,nq)
+        for (rbid,rb) in enumerate(rbs)
+            pindex = cnt[rbid]
+            ret[3nfix+rbid,pindex] .= rb.state.auxs.Φq(q[pindex])
+        end
+        ret[1,1:4] = [1.0,0.0,0.0,0.0]
+        ret[2,1:4] = [0.0,1.0,0.0,0.0]
+        ret[3,1:4] = [0.0,0.0,0.0,1.0]
+        ret
+    end
+
+    A,Φ,∂T∂q̇!,F!,M!,nothing
+end
+
+A,Φ,∂T∂q̇!,F!,M!,jacs = tail_spark(tailstruct)
+
+function initial(n,st2d)
+    rbs = st2d.rigidbodies
+    cnt = st2d.connectivity
+    n_revolute = 2 + 2(n-1)
+    ntotalbody = length(rbs)
+    nq = 4ntotalbody - 2n_revolute
+    q0 = zeros(nq)
+    for (rbid,rb) in enumerate(rbs)
+        pindex = cnt[rbid]
+        q0[pindex] .= rb.state.coords.q
+    end
+    q̇0 = zero(q0)
+    #q̇0[end] = 0.01
+    #q̇0[end-3:end] .= [0.01,0.0,0.01,0.0]
+    ninconstraint = ntotalbody
+    nfix = 1
+    nexconstraint = 3nfix
+    nconstraint = ninconstraint + nexconstraint
+    λ0 = zeros(nconstraint)
+    q0,q̇0,λ0
+end
+q0,q̇0,λ0 = initial(n,tailstruct)
+Φ(q0)
+A(q0)
+@code_warntype Φ(q0)
+@code_warntype A(q0)
+F0 = similar(q0)
+F!(F0,q0,q̇0,0.0)
+@code_warntype F!(F0,q0,q̇0,0.0)
+
+mm = zeros(length(q0),length(q0))
+M!(mm,q0)
+@code_warntype M!(mm,q0)
+
+p0 = similar(q0)
+∂T∂q̇!(p0,mm,q0,q̇0)
+@code_warntype ∂T∂q̇!(p0,mm,q0,q̇0)
+size(A(q0))
+s = 1
+tab = SPARKTableau(s)
+tspan = (0.0,20.0)
+cache = SPARKCache(size(A(q0))[2],size(A(q0))[1],0.01,tspan,s,(A,Φ,∂T∂q̇!,F!,M!,jacs))
+
+state = SPARKsolve!(q0,q̇0,λ0,cache,tab)
+
+te = [energy(state.qs[i],state.q̇s[i],rbs,vss) for i = 1:length(state.ts)]
+
+
+
+
+
+@unpack mass,inertia = rbs[1].prop
+
+Ic = inertia[2,2]
+T = 1/2*ω*Ic*ω
+
+1/2*transpose(q̇0)*M*q̇0
---
+++ src/Robot2D.jl
@@ -0,0 +1,295 @@
+module Robot2D
+
+using LinearAlgebra
+using Parameters
+using StaticArrays
+using Makie
+
+greet() = print("Hello World!")
+
+struct VString{T}
+    k::T
+    l0::MArray{Tuple{2},T,1,2}
+    lengths::MArray{Tuple{2},T,1,2}
+    tensions::MArray{Tuple{2},T,1,2}
+end
+
+struct DString{T}
+    k::T
+    original_restlengths::SArray{Tuple{4},T,1,4}
+    restlengths::MArray{Tuple{4},T,1,4}
+    lengths::MArray{Tuple{4},T,1,4}
+    tensions::MArray{Tuple{4},T,1,4}
+end
+
+struct AnchorPoint2D{T}
+    p::SArray{Tuple{2},T,1,2}
+end
+function AnchorPoint2D(sp)
+    p = SVector{2,eltype(sp)}(sp[1], sp[2])
+    AnchorPoint2D{eltype(p)}(p)
+end
+
+struct RigidBody2DProperty{T,NP}
+    movable::Bool
+    name::Symbol
+    type::Symbol
+    mass::T
+    inertia::SArray{Tuple{2,2},T,2,4}
+    CoM::SArray{Tuple{2},T,1,2}
+    anchorpoints::SArray{Tuple{NP},AnchorPoint2D{T},1,NP}
+end
+
+struct RigidBody2DState{T,NP,CoordinatesType,AuxiliariesType}
+    r::MArray{Tuple{2},T,1,2}
+    θ::T
+    ṙ::MArray{Tuple{2},T,1,2}
+    ω::T
+    p::SArray{Tuple{NP},MArray{Tuple{2},T,1,2},1,NP} # Anchor Points in global frame
+    F::MArray{Tuple{2},T,1,2}
+    τ::T
+    Fanc::SArray{Tuple{NP},MArray{Tuple{2},T,1,2},1,NP}
+    coords::CoordinatesType
+    auxs::AuxiliariesType
+end
+
+struct RigidBody2DNaturalCoordinates{T}
+    q::MArray{Tuple{4},T,1,4}
+    q̇::MArray{Tuple{4},T,1,4}
+    q̈::MArray{Tuple{4},T,1,4}
+end
+
+function RigidBody2DState(prop,ri,rj)
+    q = MVector{4}(vcat(ri,rj))
+    q̇ = zero(q)
+    q̈ = zero(q)
+    coords = RigidBody2DNaturalCoordinates(q,q̇,q̈)
+    r = MVector{2}(ri)
+    ṙ = zero(r)
+    rirj = rj-ri
+    θ = atan(rirj[2],rirj[1])
+    ω = 0.0
+    F = MVector(0.0,0.0)
+    τ = 0.0
+    f1 = MVector(0.0,0.0)
+    f2 = MVector(0.0,0.0)
+    Fanc = SVector(f1,f2)
+    τanc = MVector(0.0,0.0)
+    aux = NCaux(prop,ri,rj)
+    nap = length(prop.anchorpoints)
+    p = SVector{nap}(
+        [MVector{2}(aux.Cp[i]*q) for i in 1:nap])
+    Fanc = zero(p)
+    RigidBody2DState(r,θ,ṙ,ω,p,F,τ,Fanc,coords,aux)
+end
+struct RigidBody2D{T,NP,CoordinatesType,AuxiliariesType}
+    prop::RigidBody2DProperty{T}
+    state::RigidBody2DState{T,NP,CoordinatesType,AuxiliariesType}
+end
+
+struct NaturalCoordinatesAuxiliaries2D{T,NP,cT,ΦT,ΦqT}
+    M::SArray{Tuple{4,4},T,2,16}
+    CG::SArray{Tuple{2,4},T,2,8}
+    Cp::SArray{Tuple{NP},SArray{Tuple{2,4},T,2,8},1,NP}
+    Q::MArray{Tuple{4},T,1,4}
+    Lij::T
+    c::cT
+    Φ::ΦT
+    Φq::ΦqT
+end
+
+struct Structure2D{BodyType,StringType,ConnectType}
+    rigidbodies::Vector{BodyType}
+    strings::Vector{StringType}
+    connectivity::ConnectType
+end
+
+function inertia2Z(inertia,Lij)
+    Z = similar(inertia)
+    _Lij2 = 1/Lij^2
+    Z[1,1] = inertia[2,2]*_Lij2
+    Z[1,2] = -inertia[1,2]*_Lij2
+    Z[2,1] = Z[1,2]
+    Z[2,2] = inertia[1,1]*_Lij2
+    Z
+end
+function form_mass_matrix(m,CoM,Lij,Z)
+    M = zeros(4,4)
+    a = CoM./Lij
+    z = Z[1,1] + Z[2,2]
+    M[1,1] = m - 2m*a[1] + z
+    M[1,2] = 0.0
+    M[1,3] = m*a[1] - z
+    M[1,4] = -m*a[2]
+    M[2,2] = m - 2m*a[1] + z
+    M[2,3] = m*a[2]
+    M[2,4] = m*a[1] - z
+    M[3,3] = z
+    M[3,4] = 0.0
+    M[4,4] = z
+    SymM = Matrix(Symmetric(M))
+end
+
+function C(c)
+    ret = Matrix{eltype(c)}(undef,2,4)
+    ret[1,1] = 1-c[1]
+    ret[1,2] = c[2]
+    ret[1,3] = c[1]
+    ret[1,4] = -c[2]
+    ret[2,1] = -c[2]
+    ret[2,2] = 1-c[1]
+    ret[2,3] = c[2]
+    ret[2,4] = c[1]
+    ret
+end
+
+function NCaux(prop,ri,rj)
+    @unpack mass,CoM,inertia,anchorpoints = prop
+    Lij = norm(ri-rj)
+    Z = inertia2Z(inertia,Lij)
+    M = form_mass_matrix(mass,CoM,Lij,Z)
+    X̄⁻¹ = 1/Lij*Matrix(1.0I,2,2)
+    function c(r̄)
+        ret = X̄⁻¹*r̄
+    end
+    CG = C(c(CoM))
+    Q = zeros(4)
+    function Φ(q)
+        xi,yi,xj,yj = q
+        (xj-xi)^2 + (yj-yi)^2 - Lij^2
+    end
+    function Φq(q)
+        xi,yi,xj,yj = q
+        ret = similar(q)
+        ret[1] =  2(xi-xj)
+        ret[2] =  2(yi-yj)
+        ret[3] =  2(xj-xi)
+        ret[4] =  2(yj-yi)
+        ret
+    end
+    nap = length(anchorpoints)
+    ap = [SMatrix{2,4}(C(c(p.p))) for p in anchorpoints]
+    aux = NaturalCoordinatesAuxiliaries2D(
+    SMatrix{4,4}(M),
+    SMatrix{2,4}(CG),
+    SVector{nap}(ap),
+    MVector{4}(Q),
+    Lij,c,Φ,Φq
+    )
+end
+
+function lengthdir(v)
+    l = norm(v)
+    τ = v/l
+    l,τ
+end
+function reset_forces!(rbs)
+    for (rbid,rb) in enumerate(rbs)
+        for f in rb.state.Fanc
+            f .= 0.0
+        end
+    end
+end
+function update_forces!(st2d)
+    rbs = st2d.rigidbodies
+    vss = st2d.strings
+    cnt = st2d.connectivity
+    for (i,vs) in enumerate(vss)
+        @unpack k,restlengths,lengths,tensions = vs
+        rb1,rb2,rb3 = rbs[2i-1:2i+1]
+        P1 = rb1.state.p[1]
+        P2 = rb1.state.p[2]
+        O1 = rb2.state.p[1]
+        O2 = rb2.state.p[2]
+        P3 = rb3.state.p[1]
+        P4 = rb3.state.p[2]
+        P3P1 = P1-P3
+        P3O1 = O1-P3
+        P4P2 = P2-P4
+        P4O1 = O1-P4
+        lP3P1,τP3P1 = lengthdir(P3P1)
+        lP3O1,τP3O1 = lengthdir(P3O1)
+        lP4P2,τP4P2 = lengthdir(P4P2)
+        lP4O1,τP4O1 = lengthdir(P4O1)
+        lengths[1] = lP3P1
+        lengths[2] = lP3O1
+        lengths[3] = lP4O1
+        lengths[4] = lP4P2
+        for i = 1:4
+            Δi = lengths[i] - restlengths[i]
+            tensions[i] = ifelse(Δi > 0.0, Δi*k, 0.0)
+        end
+        fP1 = rb1.state.Fanc[1]
+        fP2 = rb1.state.Fanc[2]
+        fO1 = rb2.state.Fanc[1]
+        fP3 = rb3.state.Fanc[1]
+        fP4 = rb3.state.Fanc[2]
+        fP1 .+= -τP3P1*tensions[1]
+        fP3 .+=  τP3P1*tensions[1] + τP3O1*tensions[2]
+        fP2 .+= -τP4P2*tensions[4]
+        fP4 .+=  τP4P2*tensions[4] + τP4O1*tensions[3]
+        fO1 .+= -τP3O1*tensions[2] - τP4O1*tensions[3]
+    end
+end
+function q2rbstate!(st2d,globalq,globalq̇)
+    rbs = st2d.rigidbodies
+    cnt = st2d.connectivity
+    for (rbid,rb) in enumerate(rbs)
+        pindex = cnt[rbid]
+        @unpack q, q̇ = rb.state.coords
+        q .= globalq[pindex]
+        q̇ .= globalq̇[pindex]
+        @unpack auxs,p = rb.state
+        for (i,ap) in enumerate(p)
+            ap .= auxs.Cp[i]*q
+        end
+    end
+end
+
+function genforces!(rbs)
+    for (rbid,rb) in enumerate(rbs)
+        @unpack state = rb
+        @unpack Fanc = state
+        @unpack Q,Cp = state.auxs
+        Q .= 0.0
+        for (pid,f) in enumerate(Fanc)
+            Q .+= transpose(Cp[pid])*f
+        end
+    end
+end
+
+function kineticenergy(rbs)
+    ke = 0.0
+    for (rbid,rb) in enumerate(rbs)
+        @unpack q̇ = rb.state.coords
+        @unpack M = rb.state.auxs
+        ke += 1/2*transpose(q̇)*M*q̇
+    end
+    ke
+end
+
+function potentialenergy(vss)
+    pe = 0.0
+    for (vsid,vs) in enumerate(vss)
+        @unpack k,l0,lengths = vs
+        Δ1 = lengths[1]-l0[1]
+        Δ2 = lengths[2]-l0[2]
+        if Δ1 > 0.0
+            pe += 1/2*k*Δ1^2
+        end
+        if Δ2 > 0.0
+            pe += 1/2*k*Δ2^2
+        end
+    end
+    pe
+end
+
+function energy(q,q̇,rbs,vss)
+    q2rbstate!(rbs,q,q̇)
+    update_forces!(vss,rbs)
+    ke = kineticenergy(rbs)
+    pe = potentialenergy(vss)
+    ke + pe
+end
+
+end # module
