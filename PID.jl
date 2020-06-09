14004ec6416be9b0fe23a899d590a5156e18cfe9
Jacob <88961633+jacobleft@users.noreply.github.com>
2020-06-09 10:26:21 +0800

    initial PID controller.

---
+++ PID.jl
@@ -0,0 +1,116 @@
+module PIDController
+using Parameters
+mutable struct PID{T,limitType}
+    Kp::T
+    Ki::T
+    Kd::T
+    setpoint::T
+    dt::T
+    output_limits::limitType
+    #
+    input::T
+    output::T
+    pTerm::T
+    iTerm::T
+    dTerm::T
+    lastErr::T
+    lastInput::T
+    lastOutput::T
+    lastTime::T
+    currentTime::T
+end
+function PID(Kp,Ki,Kd;setpoint,dt=one(Kp)/100,initialInput=zero(Kp))
+    @assert typeof(Kp)==typeof(Ki)==typeof(Kd)==typeof(setpoint)
+    output_limits = nothing
+    #
+    input = zero(Kp)
+    output = zero(Kp)
+    pTerm = zero(Kp)
+    iTerm = zero(Kp)
+    dTerm = zero(Kp)
+    lastErr = zero(Kp)
+    lastInput = initialInput
+    lastOutput = zero(Kp)
+    lastTime = zero(Kp)
+    currentTime = zero(Kp)
+    PID(Kp,Ki,Kd,setpoint,
+                dt,output_limits,
+                input,output,
+                pTerm,iTerm,dTerm,
+                lastErr,lastInput,lastOutput,
+                lastTime,
+                currentTime)
+end
+
+function (pid::PID)(arg...)
+    update!(pid,arg...)
+end
+
+# function update!(pid,input;t)
+#     pid.input = input
+#     pid.currentTime = t
+#     pid.dt = t - pid.lastTime
+#     @assert pid.dt > 0.0
+#     update!(pid)
+# end
+
+function update!(pid,input;dt)
+    @assert dt > 0.0
+    pid.input = input
+    pid.dt = dt
+    pid.currentTime = pid.lastTime + pid.dt
+    update!(pid)
+end
+
+function update!(pid,input)
+    pid.input = input
+    pid.currentTime = pid.lastTime + pid.dt
+    update!(pid)
+end
+function update!(pid)
+    @unpack Kp,Ki,Kd,setpoint = pid
+    @unpack input,dt,output_limits = pid
+    # Compute all the working error variables
+    err = setpoint - input
+    #@show pid.lastInput
+    dInput = input-pid.lastInput
+    pid.pTerm = Kp*err
+    pid.iTerm += Ki*err*dt
+    pid.dTerm = -Kd*dInput/dt
+    # Compute PID Output
+    pid.output = pid.pTerm + pid.iTerm + pid.dTerm
+    if typeof(output_limits)!=Nothing
+        pid.output = clamp(pid.output,output_limits[1],output_limits[2])
+    end
+    # Remember some variables for next time
+    pid.lastOutput = pid.output
+    pid.lastInput = pid.input
+    pid.lastTime = pid.currentTime
+    return pid.output
+end
+function update!(pid,::Val{:OnMeasurement})
+    @unpack Kp,Ki,Kd,setpoint = pid
+    @unpack input,dt,output_limits = pid
+    # How long since we last calculated
+    @assert dt > 0.0
+    # Compute all the working error variables
+    err = setpoint - input
+    dInput = input-pid.lastInput
+    pid.pTerm -= Kp*dInput
+    pid.iTerm += Ki*err*dt
+    pid.dTerm = -Kd*dInput/dt
+    # Compute PID Output
+    pid.output = pid.pTerm + pid.iTerm + pid.dTerm
+    if typeof(output_limits)!=Nothing
+        pid.output = clamp(pid.output,output_limits[1],output_limits[2])
+    end
+    # Remember some variables for next time
+    pid.lastOutput = pid.output
+    pid.lastInput = pid.input
+    pid.lastTime = pid.currentTime
+    return pid.output
+end
+pid = PID(1.0,0.1,0.05;setpoint=1.0,lastInput=1.1)
+update!(pid,1.1)
+
+end
---
+++ pid_test.jl
@@ -0,0 +1,54 @@
+using Test
+@testset "trigonometric identities" begin
+           θ = 2/3*π
+           @test sin(-θ) ≈ -sin(θ)
+           @test cos(-θ) ≈ cos(θ)
+           @test sin(2θ) ≈ 2*sin(θ)*cos(θ)
+           @test cos(2θ) ≈ cos(θ)^2 - sin(θ)^2
+end;
+
+pid = PID(1, 1., 1., setpoint=0.)
+@test pid(0) == 0
+
+pid = PID(1., 0., 0., setpoint=10.)
+@test pid(0) == 10
+@test pid(5) == 5
+@test pid(-5) == 15
+
+pid = PID(1., 0., 0., setpoint=-10.)
+@test pid(0) == -10
+@test pid(5) == -15
+@test pid(-5) == -5
+@test pid(-15) == 5
+
+using DifferentialEquations
+using Plots
+pyplot()
+x0 = 1.0
+ẋ0 = 0.0
+# m*ẍ + k*x + c*ẋ = p
+# p = Kp*(setpoint-x) + Kd*(-ẋ)
+# m*ẍ + k*x + Kp*(x-setpoint) + c*ẋ + Kd*ẋ = 0
+# m*ẍ + k*x + Kp*x + c*ẋ + Kd*ẋ = Kp*setpoint
+
+function f(ẋ,x,p,t)
+    k = 1.0
+    c = 1.0
+    m = 1.0
+    λ = 1.0
+    1/m*(p - k*x - c*ẋ - λ*x^3)
+end
+function pid_maker()
+    pid = PID(1.0,1.0,1.0,setpoint=2.0)
+    function pid_affect!(intor)
+        #@show typeof(intor.u[1]),intor.t
+        output = update!(pid,intor.u[2];dt=intor.t-intor.tprev)
+        intor.p = output
+    end
+end
+pid_callback = DiscreteCallback((u,t,integrator)->true,pid_maker())
+prob = SecondOrderODEProblem(f,ẋ0,x0,(0.0,100.0),0.0,callback=pid_callback)
+sol = solve(prob,DPRKN6(),dt=0.01,adaptive=false)
+
+plot(sol,label=["ẋ" "x"])
+ylims!(-1,3)
