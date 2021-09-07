eec09208167ac791be1b4953e5e7811137fe85ec
Jacob <88961633+jacobleft@users.noreply.github.com>
2021-09-07 22:58:50 +0800

    添加滑动绳索类的构建

--- src/string.jl
+++ src/string.jl
@@ -65,3 +65,41 @@ function SMAString3D(id,restlen::T,law::LawT,c::T,original_temp::T=0.0) where {L
     state = SMAStringState(original_temp,restlen,direction)
     SMAString(id,law,c,state)
 end
+
+mutable struct ClusterSectionCablesState{N, T}
+    restlen::T
+    length::T 
+    lengthdot::T
+    tension::T
+    direction::MArray{Tuple{N},T,1,N}
+end
+
+struct ClusterSectionCables{N, T}
+    ID::Int
+    state::ClusterSectionCablesState{N, T}
+end
+
+mutable struct ClusterCablesState{T}
+    theta::Vector{T}
+    s::Vector{T}
+end
+
+struct ClusterCables
+    ID::Int
+    state::ClusterCablesState
+    section::Vector{ClusterSectionCables}
+end
+
+function ClusterSectionCables3D(id, restlen::T) where T
+    direction = MVector{3}(one(T),zero(T),zero(T))
+    section_state = ClusterSectionCablesState(restlen, restlen, 
+        zero(restlen), zero(restlen), direction)
+    return ClusterSectionCables(id, section_state)
+end
+
+function ClusterCables3D(Vector_Cluster_Section, section, id) 
+    s = [Float64(0.0) for i in 1:section]
+    theta = similar(s)
+    state = ClusterCablesState(theta, s)
+    return ClusterCables(id, state, Vector_Cluster_Section)
+end
